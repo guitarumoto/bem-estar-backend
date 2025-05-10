94eb05a30d953474561cc52cfe86bf19a757c822
emanuele-rosa <emanuele.rosa@startse.com>
2025-05-10 19:12:15 -0300

    fix: corrigir caminho de destino no Vercel e ajustar inicialização do servidor

--- src/server.ts
+++ src/server.ts
@@ -14,13 +14,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (_, res) => {
+  res.json({ message: 'API Ok!' });
+});
+
 app.use('/api/users', userRoutes);
 app.use('/api/moods', moodRoutes);
 app.use('/api/goals', goalRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-}); 
+export default app;
